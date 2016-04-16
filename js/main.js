b159336da3f96df372fff49c0c3f5f612d0df41e
Andrew-DiBella <adibella@ufl.edu>
2016-04-15 21:54:59 -0700

    add menu link styles and more javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var welcome = document.getElementById( 'welcome' );
 
 welcome.addEventListener( 'click', function() {
-	welcome.innerHTML = '<h2>Youll love Pizza Planet!</h2>';
+	welcome.innerHTML = '<h3>You will love Pizza Planet!</h3>';
 } );
