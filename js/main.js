9bbb561cdaf719b1fcdbe108fefe9b48edc2432b
Andrew-DiBella <adibella@ufl.edu>
2016-04-16 15:55:58 -0700

    add animate scroll plugin

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ var welcome = document.getElementById( 'welcome' );
 welcome.addEventListener( 'click', function() {
 	welcome.innerHTML = '<h3>You will love Pizza Planet!</h3>';
 } );
+
+//SMINT Plug-in
+
+
+$( document ).ready( function() {
+	$( '#welcome' ).animatescroll( ( { scrollSpeed:2000, easing:'easeOutBounce' } ) );
+	$( '#menu' ).animatescroll( ( { scrollSpeed:2000, easing:'easeOutBounce' } ) );
+	$( '#specials' ).animatescroll( ( { scrollSpeed:2000, easing:'easeOutBounce' } ) );
+	$( '#contact' ).animatescroll( ( { scrollSpeed:2000, easing:'easeOutBounce' } ) );
+} );
